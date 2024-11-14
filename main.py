1c003ea7138218d2c582886cfcf19cd1b8bd409d
Billy Chan <billycychan.work@gmail.com>
2024-11-14 12:13:00 -0500

    feat: add agent_scratchpad

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 from typing import Union, List
-
 from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
-from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
 from langchain.prompts import PromptTemplate
 from langchain.schema import AgentAction, AgentFinish
-from langchain.tools import Tool, tool
+from langchain.tools import Tool
 from langchain.tools.render import render_text_description
+from callbacks import AgentCallbackHandler
 
 load_dotenv()
 
 
-# This function calculates the length of a given text by characters.
 @tool
 def get_text_length(text: str) -> int:
     """Returns the length of a text by characters"""
     print(f"get_text_length enter with {text=}")
     text = text.strip("'\n").strip(
         '"'
-    )  # stripping away non alphabetic characters just in case
+    )  # stripping away non-alphabetic characters just in case
 
     return len(text)
-
-
-"""
-find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool: 
-This function searches for a tool by its name within a list of tools and returns it. 
-If the tool is not found, it raises a ValueError.
-"""
 
 
 def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
@@ -62,44 +54,52 @@ if __name__ == "__main__":
     Begin!
 
     Question: {input}
-    Thought:
+    Thought: {agent_scratchpad}
     """
 
-    # The template is rendered with the available tools and their names.
     prompt = PromptTemplate.from_template(template=template).partial(
         tools=render_text_description(tools),
         tool_names=", ".join([t.name for t in tools]),
     )
 
     llm = ChatOpenAI(
-        model="gpt-4o-mini", temperature=0, stop=["\nObservation", "Observation"]
+        model="gpt-4o-mini",
+        temperature=0,
+        stop=["\nObservation", "Observation"],
     )
-    # llm = ChatOllama(
-    #     model="llama3", temperature=0, stop=["\nObservation", "Observation"]
-    # )
     intermediate_steps = []
-
-    # An agent is set up to process the input, generate thoughts, actions, and observations.
     agent = (
-        {
-            "input": lambda x: x["input"],
-        }
-        | prompt
-        | llm
-        | ReActSingleInputOutputParser()
+            {
+                "input": lambda x: x["input"],
+                "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
+            }
+            | prompt
+            | llm
+            | ReActSingleInputOutputParser()
     )
 
     agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
         {
-            "input": "What is the length of characters of the text DOG ?",
+            "input": "What is the length of the word: DOG",
+            "agent_scratchpad": intermediate_steps,
         }
     )
     print(agent_step)
-
     if isinstance(agent_step, AgentAction):
         tool_name = agent_step.tool
         tool_to_use = find_tool_by_name(tools, tool_name)
         tool_input = agent_step.tool_input
-
         observation = tool_to_use.func(str(tool_input))
         print(f"{observation=}")
+        intermediate_steps.append((agent_step, str(observation)))
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length of the word: DOG",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
+    print(agent_step)
+    if isinstance(agent_step, AgentFinish):
+        print("### AgentFinish ###")
+        print(agent_step.return_values)
